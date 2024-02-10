e4d741477ee9496ab66be780a0f687bf53b46ad0
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-02-10 21:13:13 +0100

    js hm 9 1

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+function readFormData(form) {
+  const email = form.email.value;
+  const message = form.message.value;
+  return {
+    email,
+    message,
+  };
+}
+
+form.addEventListener('input', event => {
+  event.preventDefault();
+  const data = readFormData(event.currentTarget);
+  const jsonData = JSON.stringify(data);
+  localStorage.setItem(STORAGE_KEY, jsonData);
+});
+
+const inputData = localStorage.getItem(STORAGE_KEY);
+if (inputData) {
+  const data = JSON.parse(inputData);
+  form.email.value = data.email;
+  form.message.value = data.message;
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  console.log(readFormData(form));
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
